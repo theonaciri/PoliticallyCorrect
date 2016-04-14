f933aa7ea8a7b97c2d40b5cbded55d9f86d0fe05
Theo Naciri <theo.naciri@gmail.com>
2016-04-14 12:48:04 +0530

    add candidate

--- .tmp/public/js/public/poll/PollController.js
+++ .tmp/public/js/public/poll/PollController.js
@@ -1,91 +1,93 @@
 angular.module('PollModule')
 .controller('PollController', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.title = "Poll";
+	$scope.title = "Poll";
     //$scope.user.name = "Theo"
     $scope.toggleLeft = buildDelayedToggler('left');
     $scope.toggleRight = buildToggler('right');
     $scope.isOpenRight = function(){
-      return $mdSidenav('right').isOpen();
+    	return $mdSidenav('right').isOpen();
     };
     /**
      * Supplies a function that will continue to operate until the
      * time is up.
      */
-    function debounce(func, wait, context) {
-      var timer;
-      return function debounced() {
-        var context = $scope,
-            args = Array.prototype.slice.call(arguments);
-        $timeout.cancel(timer);
-        timer = $timeout(function() {
-          timer = undefined;
-          func.apply(context, args);
-        }, wait || 10);
-      };
-    }
+     function debounce(func, wait, context) {
+     	var timer;
+     	return function debounced() {
+     		var context = $scope,
+     		args = Array.prototype.slice.call(arguments);
+     		$timeout.cancel(timer);
+     		timer = $timeout(function() {
+     			timer = undefined;
+     			func.apply(context, args);
+     		}, wait || 10);
+     	};
+     }
     /**
      * Build handler to open/close a SideNav; when animation finishes
      * report completion in console
      */
-    function buildDelayedToggler(navID) {
-      return debounce(function() {
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      }, 200);
-    }
-    function buildToggler(navID) {
-      return function() {
-        $mdSidenav(navID)
-          .toggle()
-          .then(function () {
-            $log.debug("toggle " + navID + " is done");
-          });
-      }
-    }
-  })
+     function buildDelayedToggler(navID) {
+     	return debounce(function() {
+     		$mdSidenav(navID)
+     		.toggle()
+     		.then(function () {
+     			$log.debug("toggle " + navID + " is done");
+     		});
+     	}, 200);
+     }
+     function buildToggler(navID) {
+     	return function() {
+     		$mdSidenav(navID)
+     		.toggle()
+     		.then(function () {
+     			$log.debug("toggle " + navID + " is done");
+     		});
+     	}
+     }
+ })
 .controller('DatePicker', function($scope) {
-  $scope.startDate = new Date();
-  $scope.minDate = new Date(
-      $scope.startDate.getFullYear(),
-      $scope.startDate.getMonth(),
-      $scope.startDate.getDate());
-  $scope.maxDate = new Date(
-      $scope.startDate.getFullYear(),
-      $scope.startDate.getMonth() + 2,
-      $scope.startDate.getDate());
-  $scope.onlyWeekendsPredicate = function(date) {
-    var day = date.getDay();
-    return day === 0 || day === 6;
-  }
+	$scope.startDate = new Date();
+	$scope.minDate = new Date(
+		$scope.startDate.getFullYear(),
+		$scope.startDate.getMonth(),
+		$scope.startDate.getDate());
+	$scope.maxDate = new Date(
+		$scope.startDate.getFullYear(),
+		$scope.startDate.getMonth() + 3,
+		$scope.startDate.getDate());
 })
-  .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      $mdSidenav('left').close()
-        .then(function () {
-          $log.debug("close LEFT is done");
-        });
-    };
-  })
-  .controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
-    $scope.close = function () {
-      $mdSidenav('right').close()
-        .then(function () {
-          $log.debug("close RIGHT is done");
-        });
-    }})
-  .controller('CandidatesCtrl', function($scope) {
-  	$scope.candidates = [];
-  	$scope.keypress = function($event, button_clicked) {
-    if (button_clicked || $event.keyCode == 13) {
-    		$scope.candidates.push({name: $scope.candidate, desc: $scope.can_desc});
-    		$scope.candidate = '';
-    		$scope.can_desc = '';
-    	}
-  	};
-  	$scope.removeCandidate = function($index, item) {
-    $scope.candidates.splice($index, 1);    
-  };
-  });
+.controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+	$scope.close = function () {
+		$mdSidenav('left').close()
+		.then(function () {
+			$log.debug("close LEFT is done");
+		});
+	};
+})
+.controller('RightCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+	$scope.close = function () {
+		$mdSidenav('right').close()
+		.then(function () {
+			$log.debug("close RIGHT is done");
+		});
+	}})
+.controller('CandidatesCtrl', function($scope) {
+	$scope.candidates = [];
+	$scope.desc_style = {'display': 'none'};
+	$scope.createCandidate = function($event, from_button) {
+		if ((from_button || $event.keyCode == 13) && $scope.candidate) {
+			$scope.candidates.push({name: $scope.candidate, desc: $scope.can_desc});
+			$scope.candidate = '';
+			$scope.can_desc = '';
+		}
+		if ($scope.candidate) {
+			$scope.desc_style = {'/*display': 'initial*/'};
+		}
+		else
+			$scope.desc_style = {'display': 'none'};
+	};
+	$scope.removeCandidate = function($index, item) {
+		$scope.candidates.splice($index, 1);
+	};
+});
