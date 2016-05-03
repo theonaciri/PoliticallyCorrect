d7f28d20fbb78ff276cdffd4d18d67ddf209c9f2
Theo Naciri <theo.naciri@gmail.com>
2016-05-03 18:19:44 +0530

    add all votes, not aggragated yet

--- api/services/CountVotesService.js
+++ api/services/CountVotesService.js
@@ -2,14 +2,14 @@
  * Created by naciri_t on 5/2/16.
  */
 var PythonShell = require('python-shell');
-var fs = require('fs');
-
-var path = require('path');
+/*
+ var _ = require('underscore-min.js');
+ */
 
 module.exports = {
 
     countVotes: function(options) {
-        /*        sails.log(options);*/
+        var _ = sails._;
         var i = 0;
         var choices = [];
         while (options[i]) {
@@ -17,22 +17,50 @@ module.exports = {
             i++;
         }
         sails.log(choices);
-        fs.stat('foo.txt', function(err, stat) {
-            if(err == null) {
-                sails.log('File exists');
-            } else if(err.code == 'ENOENT') {
-                fs.writeFile('log.txt', 'Some log\n');
-            } else {
-                sails.log('Some other error: ', err.code);
+
+        sails.log(_.isEqual(choices[0], choices[1]));
+
+        var summed_array = [];
+
+        choices.forEach(function (vote) {
+            console.log("testing vote : ", vote);
+            if (_.isEmpty(summed_array)) {
+                summed_array.push({"count":1, "ballot": vote});
+            }
+            else {
+                summed_array.forEach(function(summed) {
+                    console.log('looping vor summed : ', summed);
+                    if (_.isEqual(vote.ballot, summed)) {
+                        console.log("same, updating : ", summed);
+
+                    } else {
+                        console.log("not the same, pushing:", summed);
+                        summed_array.push({"count":1, "ballot": vote});
+                    }
+                });
             }
         });
-/*        fs.exists('/home/naciri_t/Projects/PoliticallyCorrect/api/services/test_stv.py', (exists) => {
-            console.log(exists ? 'it\'s there' : 'no passwd!');
-    })*/
-        PythonShell.run('echo_args.py', {scriptPath: '/home/naciri_t/Projects/PoliticallyCorrect/node_modules/python-shell/test/python/', pythonPath: '/usr/bin/python2.7'} , function (err) {
-            if (err) throw err;
-            console.log('finished');
-        });
+        console.log('summed array : ', summed_array);
+
+        /*        _.each(arr2, function(arr2obj) {
+         var arr1obj = _.find(arr1, function(arr1obj) {
+         return arr1obj[prop] === arr2obj[prop];
+         })});
+         */
+        var options = {
+            pythonPath: '/usr/bin/python2.7',
+            scriptPath: '/home/naciri_t/Projects/PoliticallyCorrect/api/services',
+            args: choices
+        };
+
+        /*        PythonShell.run('test_stv.py', options, function (err) {
+         if (err) throw err;
+         console.log('finished');
+         });*/
         return choices;
     }
 };
+
+/*        fs.exists('/home/naciri_t/Projects/PoliticallyCorrect/api/services/test_stv.py', (exists) => {
+ console.log(exists ? 'it\'s there' : 'no passwd!');
+ })*/
